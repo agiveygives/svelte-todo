030e008fac6f3f73c423c9eede8d50923adf702b
agiveygives <agivens@justworks.com>
2022-05-31 14:13:04 -0500

    create a component

---
+++ 2.create-a-component/src/components/TodoItem.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let item: string;
+</script>
+
+<div>
+  <input type='checkbox' />
+
+  <span>{item}</span>
+</div>
---
+++ 2.create-a-component/src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+  import TodoItem from '../components/TodoItem.svelte';
+
+  let todoItems = [
+    'Generate Project Skeleton',
+    'Create your first component',
+    'Create a store',
+    'Make a new todo items flow'
+  ]
+</script>
+
+{#each todoItems as todoItem}
+  <TodoItem item={todoItem} />
+{/each}

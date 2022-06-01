b0740cf793dcfc0cd92a448afc25c4b6aa681d2e
agiveygives <agivens@justworks.com>
2022-06-01 08:12:39 -0500

    create store and update functions

---
+++ 3.svelte-stores/src/components/AddTodoItem.svelte
@@ -0,0 +1,21 @@
+<script lang='ts'>
+  import { todoItems } from '../stores/todo';
+  import type { TodoItemType } from '../stores/todo';
+
+  let todoItem = '';
+
+  const addNewTask = () => {
+    todoItems.update((store: TodoItemType[]) => {
+      store.push({
+        item: todoItem,
+        done: false
+      })
+
+      return store;
+    })
+  }
+</script>
+
+<input type='text' bind:value={todoItem} />
+
+<button on:click={addNewTask}>Add</button>
---
+++ 3.svelte-stores/src/components/TodoItem.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { todoItems } from '../stores/todo';
+  import type { TodoItemType } from '../stores/todo';
+
+  export let item: string;
+  export let done: boolean = false;
+
+  const toggleTaskCompletion = () => {
+    todoItems.update((store: TodoItemType[]) => {
+      const updateIndex = store
+        .map<string>((todoItem: TodoItemType) => todoItem.item)
+        .indexOf(item)
+
+      store[updateIndex] = {
+        item,
+        done: !done
+      }
+
+      return store;
+    })
+  }
+</script>
+
+<div>
+  <input
+    type='checkbox'
+    bind:checked={done}
+    on:click={toggleTaskCompletion}
+  />
+
+  <span>{item}</span>
+</div>
